fe066f1a6c74ea8d5ff1ca109e1a439d32d5da52
Ksenia Golovchik <k.golovchik@godeltech.com>
2019-06-18 15:23:14 +0300

    ss

--- arduino/src/index.js
+++ arduino/src/index.js
@@ -1,24 +1,34 @@
 const five = require('johnny-five');
-let board;
-
 const request = require('request');
 const CronJob = require('cron').CronJob;
-const job = new CronJob('*/5 * * * * *', () => {
-  if (!board) board = new five.Board();
 
-  request('https://silere-button.herokuapp.com/api/light', { json: true }, (err, res, body) => {
+let board = new five.Board();
+let ready = false;
+let relay;
+function inject() {
+  this.repl.inject({ relay });
+}
+
+function toggle(light) {
+  if (!ready) return;
+
+  if (light) relay.on();
+  else relay.off();
+
+  inject();
+}
+const job = new CronJob('*/5 * * * * *', () => {
+  request('https://silere-button.herokuapp.com/api/light', { json: true }, (err, res, { light }) => {
     if (err) { return console.log(err); }
 
     console.log('LIGHT = ', body.light);
 
-    board.on('ready', function() {
-      const relay = new five.Relay(8);
-      if (body.light) relay.off();
-      else relay.off();
+    toggle(light);
 
-      this.repl.inject({
-        relay: relay
-      });
+    board.on('ready', () => {
+      relay = new five.Relay(8);
+      inject = inject.bind(this);
+      ready = true;
     });
   });
 });
